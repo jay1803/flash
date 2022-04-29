94fdb8e01023b4c5d1a38b1545e25ca5527ca0ef
max <github.com@maxos.email>
2022-04-29 19:45:02 +0800

    add text filed view

---
+++ Shared/View/NoteEditor.swift
@@ -0,0 +1,37 @@
+//
+//  NoteEditor.swift
+//  flash
+//
+//  Created by Max Zhang on 2022/4/29.
+//
+
+import SwiftUI
+
+struct NoteEditor: View {
+    @State private var inputText: String = ""
+    var body: some View {
+        HStack {
+            TextField("Note content...", text: $inputText)
+                .frame(height: 50)
+                .padding(.leading)
+                .padding(.trailing)
+                .cornerRadius(10)
+                .border(Color(UIColor.separator))
+                
+            Button(action: {
+                print("Send button pressed...")
+            }) {
+                Label("Send", systemImage: "paperplane.fill")
+            }
+        }
+        .padding(.horizontal, 10)
+        .padding(.vertical, 5)
+    }
+}
+
+struct NoteEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteEditor()9
+            .scaledToFit()
+    }
+}
