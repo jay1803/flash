b99eff3cb9d9ccbc28bee7f7e490bc68ec9de807
max <github.com@maxos.email>
2022-05-31 13:23:09 +0800

    add entryEditor UI test

--- Tests iOS/Tests_EntryList.swift
+++ Tests iOS/Tests_EntryList.swift
@@ -11,21 +11,24 @@ import ViewInspector
 extension Home: Inspectable { }
 extension EmptyEntry: Inspectable { }
 extension EntryList: Inspectable { }
+extension EntryEditor: Inspectable { }
+extension TextInput: Inspectable { }
 
 class Tests_EntryList: XCTestCase {
     
     let app = XCUIApplication()
     let realmManager = RealmManager(name: "testFlash")
     let homeView = Home()
+    
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        realmManager.deleteAll()
-        app.launch()
         ViewHosting.host(view: homeView.environmentObject(realmManager))
+        app.launch()
+        
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -37,22 +40,39 @@ class Tests_EntryList: XCTestCase {
 
     func test_EntryList_Init_ExpectToSeeEmptyList() throws {
         // Given
-        let entryListView = try homeView.inspect().view(EntryList.self)
-        let emptryEntryView = try entryListView.zStack().view(EmptyEntry.self, 0)
-        let emtryEntryText = try emptryEntryView.geometryReader().text().string()
+        ViewHosting.host(view: homeView.environmentObject(realmManager))
+        let entryListView = try homeView.inspect().zStack().view(EntryList.self, 0)
+        let emptryEntryView = try entryListView.view(EmptyEntry.self)
+        let emtryEntryText = try emptryEntryView.geometryReader().text()
 
         // Then
         XCTAssertFalse(entryListView.isAbsent, "Expected to see entryListView")
         XCTAssertFalse(emptryEntryView.isAbsent, "Expected to see emptryEntryView")
-        XCTAssertEqual(emtryEntryText, "Start to add some notes here...")
+        XCTAssertFalse(emtryEntryText.isAbsent, "Expected to see emptryEntryView")
+        XCTAssertEqual(try emtryEntryText.string(), "Start to add some notes here...")
     }
 
-    func test_TextEidotr_Init_ExpectHeight() throws {
+    func test_TextEditor_Init_ExpectHeight() throws {
         // Given
-        
-        // When
+        ViewHosting.host(view: homeView.environmentObject(realmManager))
+        let editorGroup = try homeView.inspect().zStack().view(EntryEditor.self, 1)
+        let editorView = try editorGroup.vStack().hStack(1)
+        let textInput = try editorView.view(TextInput.self, 1)
+        let editorHeight = try editorView.fixedHeight()
+        // Then
+        XCTAssertFalse(editorView.isAbsent)
+        XCTAssertEqual(editorHeight, 36)
+        XCTAssertFalse(textInput.isAbsent, "Expected to see text input.")
+    }
+    
+    func test_TextEditor_HeightChanges() throws {
+        ViewHosting.host(view: homeView.environmentObject(realmManager))
+        let editorGroup = try homeView.inspect().zStack().view(EntryEditor.self, 1)
+        let editorView = try editorGroup.vStack().hStack(1)
+        let textInput = try editorView.view(TextInput.self, 1)
         
         // Then
-            
+        XCTAssertFalse(textInput.isAbsent, "Expected to see text input.")
+        
     }
 }
