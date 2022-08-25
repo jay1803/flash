8ceebca60ddd4a164f5d076ac7798dc320d7992a
max <github.com@maxos.email>
2022-08-25 14:02:31 +0800

    💄 update quote editor layout.

--- Shared/View/Editor/QuoteEditor.swift
+++ Shared/View/Editor/QuoteEditor.swift
@@ -15,16 +15,31 @@ struct QuoteEditor: View {
     @State var parentEntry: Entry? = nil
     
     var body: some View {
-        VStack {
-            Text(quoteContent)
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading) {
+                Text(quoteContent)
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 8)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 7)
+                            .stroke(Color.gray, style: StrokeStyle(lineWidth: 1))
+                }
+                Text("Reply with quote: ")
+                    .font(.caption)
+            }
+            .padding(.horizontal, 12)
+            
+            Divider()
+            
             TextEditor(text: $entryContent)
-                .padding()
+                .padding(.horizontal, 12)
         }
     }
 }
 
 struct QuoteEditor_Previews: PreviewProvider {
     static var previews: some View {
-        QuoteEditor(quoteContent: "This is a quote", entryContent: .constant("This is a entry content"))
+        QuoteEditor(quoteContent: "This is a quote. This is a quote. This is a quote. This is a quote",
+                    entryContent: .constant("This is a reply, This is a reply, This is a reply. This is a reply"))
     }
 }
